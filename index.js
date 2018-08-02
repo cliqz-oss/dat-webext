f5e3f6de98b09b1c8a9d51dedf2d2fbe700616a9
Sam Macbeth <sam@cliqz.com>
2018-08-02 11:04:57 +0200

    Separate protocol implementation and handle edge cases.

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const parseUrl = require('parse-dat-url');
 const Spanan = require('spanan').default;
 const DatArchive = require('dat-archive-web');
 const DefaultManager = DatArchive.DefaultManager
+const protocolHandler = require('./protocol');
 
 class Manager extends DefaultManager {
   getStorage(key) {
     // return ram;
     const storage = randomAccessIdb(key, { idb: global.indexedDB });
     return storage;
+  }
+  onAddArchive(key, secretKey, options) {
+    console.log('add archive', key, secretKey, options);
   }
 }
 DatArchive.setManager(new Manager('http://localhost:3002'));
@@ -28,93 +32,19 @@ function getArchiveFromUrl(url) {
   return getArchive(host);
 }
 
-class StreamIterator {
-  constructor(stream) {
-    this.stream = stream;
-    this.state = 'open';
-    this.chunks = [];
-    this.waitingResolve = null;
-    this.waitingReject = null;
-    stream.on('close', () => {
-      this.state = 'closed';
-      if (this.waitingResolve) {
-        this.waitingResolve(false);
-      }
-    });
-    stream.on('end', () => {
-      this.state = 'end';
-      if (this.waitingResolve) {
-        this.waitingResolve(false);
-      }
-    });
-    stream.on('error', (e) => {
-      this.state = 'error'
-      this.error = e;
-      if (this.waitingReject) {
-        this.waitingReject(this.error);
-      }
-    });
-    stream.on('data', (chunk) => {
-      if (this.waitingResolve !== null) {
-        const resolve = this.waitingResolve;
-        this.waitingReject = null;
-        this.waitingResolve = null;
-        resolve(chunk);
-      } else {
-        this.chunks.push(chunk);
-      }
-    });
-  }
-
-  async next() {
-    if (this.chunks.length > 0) {
-      return Promise.resolve(this.chunks.shift());
-    } else if (this.state === 'error') {
-      return Promise.reject(this.error);
-    } else if (this.state != 'open') {
-      return Promise.resolve(false);
-    }
-    return new Promise((resolve, reject) => {
-      this.waitingResolve = resolve;
-      this.waitingReject = reject;
-    });
-  }
-}
-
 browser.protocol.registerProtocol('dat', (request) => {
-  const url = parseUrl(request.url);
-  const archive = getArchive(url.host);
-  return {
-    content: (async function*() {
-      try {
-        const stat = await archive.stat(url.pathname, { timeout: 30000 });
-        let path = url.pathname;
-        if (stat.isDirectory()) {
-          path = `${path}index.html`;
-        }
-        // const contents = await archive.readFile(path, { encoding: 'binary'});
-        const stream = archive._archive.createReadStream(path, { start: 0 });
-        const streamIt = new StreamIterator(stream);
-        while(true) {
-          const next = await streamIt.next();
-          if (!next) {
-            break;
-          }
-          yield next.buffer;
-        }
-      } catch(e) {
-        const encoder = new TextEncoder('utf8');
-        yield encoder.encode(`Error: ${e}`).buffer;
-      }
-    })()
-  };
+  return protocolHandler.handleRequest(request, { getArchive });
 });
 
 const client = 'cliqz@cliqz.com';
 const apiWrapper = new Spanan();
-apiWrapper.export({
+const api = {
   resolveName(name) {
     return DatArchive.resolveName(name);
+  },
+  async create(opts) {
+    const archive = await DatArchive.create(opts);
+    return archive.url;
   },
   getInfo(url, opts) {
     const archive = getArchiveFromUrl(url);
@@ -143,7 +73,13 @@ apiWrapper.export({
     const archive = getArchiveFromUrl(url);
     return archive.history(opts);
   },
-}, {
+  writeFile(url, data, opts) {
+    const archive = getArchiveFromUrl(url);
+    return archive.writeFile(data, opts);
+  }
+};
+global.api = api;
+apiWrapper.export(api, {
   respond(response, request) {
     browser.runtime.sendMessage(client, {
       uuid: request.uuid,
---
+++ protocol.js
@@ -0,0 +1,149 @@
+const parseUrl = require('parse-dat-url');
+const pda = require('pauls-dat-api');
+const joinPaths = require('path').join;
+
+class StreamIterator {
+  constructor(stream) {
+    this.stream = stream;
+    this.state = 'open';
+    this.chunks = [];
+    this.waitingResolve = null;
+    this.waitingReject = null;
+    stream.on('close', () => {
+      this.state = 'closed';
+      if (this.waitingResolve) {
+        this.waitingResolve(false);
+      }
+    });
+    stream.on('end', () => {
+      this.state = 'end';
+      if (this.waitingResolve) {
+        this.waitingResolve(false);
+      }
+    });
+    stream.on('error', (e) => {
+      this.state = 'error'
+      this.error = e;
+      if (this.waitingReject) {
+        this.waitingReject(this.error);
+      }
+    });
+    stream.on('data', (chunk) => {
+      if (this.waitingResolve !== null) {
+        const resolve = this.waitingResolve;
+        this.waitingReject = null;
+        this.waitingResolve = null;
+        resolve(chunk);
+      } else {
+        this.chunks.push(chunk);
+      }
+    });
+  }
+
+  async next() {
+    if (this.chunks.length > 0) {
+      return Promise.resolve(this.chunks.shift());
+    } else if (this.state === 'error') {
+      return Promise.reject(this.error);
+    } else if (this.state != 'open') {
+      return Promise.resolve(false);
+    }
+    return new Promise((resolve, reject) => {
+      this.waitingResolve = resolve;
+      this.waitingReject = reject;
+    });
+  }
+}
+
+function responseText(string) {
+  const encoder = new TextEncoder('utf8');
+  return encoder.encode(string).buffer;
+}
+
+module.exports = {
+  handleRequest(request, { getArchive }) {
+    const { host, pathname, version, search } = parseUrl(request.url);
+    const archive = getArchive(host);
+    return {
+      content: (async function* () {
+        try {
+          let filePath = pathname;
+          let isFolder = filePath.endsWith('/');
+          const manifest = await pda.readManifest(archive._archive).catch(_ => { });
+
+          let entry = null;
+          const tryStat = async (testPath) => {
+            // abort if we've already found it
+            if (entry) return
+            // apply the web_root config
+            if (manifest && manifest.web_root) {
+              if (testPath) {
+                testPath = joinPaths(manifest.web_root, testPath)
+              } else {
+                testPath = manifest.web_root
+              }
+            }
+            // attempt lookup
+            try {
+              entry = await archive.stat(testPath)
+              entry.path = testPath;
+            } catch (e) {
+              console.error('stat error', testPath, e);
+            }
+          }
+
+          if (!isFolder) {
+            await tryStat(filePath)
+            if (entry && entry.isDirectory()) {
+              entry = null;
+              await tryStat(`${filePath}/${search}`);
+              if (entry && entry.isDirectory()) {
+                isFolder = true;
+                filePath = `${filePath}/${search}`;
+                entry = null;
+              }
+            }
+          }
+
+          if (isFolder) {
+            await tryStat(filePath + 'index.html')
+            await tryStat(filePath + 'index.md')
+            await tryStat(filePath)
+          } else {
+            await tryStat(filePath)
+            await tryStat(filePath + '.html') // fallback to .html
+          }
+
+          if (entry && entry.isDirectory()) {
+            yield responseText(`Directory ${entry.path}`);
+            return;
+          }
+
+          if (!entry) {
+            // check for a fallback page
+            if (manifest && manifest.fallback_page) {
+              await tryStat(manifest.fallback_page)
+            }
+
+            if (!entry) {
+              yield responseText(`Error: Not found`);
+              return;
+            }
+          }
+
+          const stream = archive._archive.createReadStream(entry.path, { start: 0 });
+          const streamIt = new StreamIterator(stream);
+          while (true) {
+            const next = await streamIt.next();
+            if (!next) {
+              break;
+            }
+            yield next.buffer;
+          }
+        } catch (e) {
+          yield responseText(`Error: ${e}`);
+        }
+      })()
+    };
+  },
+}
