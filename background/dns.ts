1d45f0a92f0ffa0049aa30c29cb7f6e93ffe3ff9
Sam Macbeth <sam@macbeth.cc>
2019-08-31 10:10:54 +0200

    Switch to cloudflare dns

--- background/dns.ts
+++ background/dns.ts
@@ -62,7 +62,7 @@ class DNSCache {
 const lookupCache = new DNSCache();
 const datDns = datDnsFactory({
   persistentCache: lookupCache,
-  dnsHost: 'dns.quad9.net:5053',
+  dnsHost: 'cloudflare-dns.com',
   dnsPath: '/dns-query',
 });
 
