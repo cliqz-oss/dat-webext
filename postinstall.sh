94b4ac80752c995444924f408551fdfd06faf056
Sam Macbeth <sammacbeth@users.noreply.github.com>
2019-12-19 15:35:11 +0100

    Fixing protocol handler on nightly (#75)
    * Add nightly back to test matrix
    
    * Bump libdweb
    
    * Add libdweb patch
    
    * Bump to fixed libdweb version
    
    * Use patch-package for patching.

--- postinstall.sh
+++ postinstall.sh
@@ -9,11 +9,3 @@ cp node_modules/bulma/css/* addon/assets/
 # Remove 'browser' attribute in hyperswarm so we can compile a webext compatibile version.
 mv ./node_modules/hyperdiscovery/package.json ./node_modules/hyperdiscovery/package.web.json && \
     node -e "var p = require(\"./node_modules/hyperdiscovery/package.web.json\"); delete p.browser; console.log(JSON.stringify(p, null, \"  \"));" > ./node_modules/hyperdiscovery/package.json
-# Fix bug with nextTick in hypercore
-sed -i.bak "s/require('process-nextick-args')/process.nextTick/g" ./node_modules/hypercore/index.js && rm ./node_modules/hypercore/index.js.bak
-# Replace utp-native with utp-wasm in discovery-swarm
-sed -i.bak "s/require('utp-native')/require('\@sammacbeth\/utp-wasm')/g" ./node_modules/discovery-swarm/index.js && rm ./node_modules/discovery-swarm/index.js.bak
-# Require path.posix in hyperdrive
-sed -i.bak "s/require('path').posix/require('path')/g" ./node_modules/hyperdrive/index.js && rm ./node_modules/hyperdrive/index.js.bak
-# Reduce dns cache clean interval
-sed -i.bak "s/options.interval/300/g" ./node_modules/dat-dns/cache.js && rm ./node_modules/dat-dns/cache.js.bak
